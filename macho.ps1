23e143936f10d179b22d6b7074a76c846534cf94
MOG. <puddycat20@hotmail.com>
2026-06-27 16:55:10 +0100

    Create macho.ps1

---
+++ macho.ps1
@@ -0,0 +1,48 @@
+$Source = @"
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class KeyboardHook {
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private static LowLevelKeyboardProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
+
+    public static void Start() { _hookID = SetHook(_proc); Application.Run(); }
+    public static void Stop() { UnhookWindowsHookEx(_hookID); }
+
+    private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+        using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
+        using (var curModule = curProcess.MainModule) {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc, IntPtr.Zero, 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+            int vkCode = Marshal.ReadInt32(lParam);
+            if (vkCode == 162) { // 162 is Left Control
+                SendKeys.SendWait("C:\\");
+                return (IntPtr)1; // Block the original Ctrl key press
+            }
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+}
+"@
+
+Add-Type -TypeDefinition $Source -ReferencedAssemblies "System.Windows.Forms"
+[KeyboardHook]::Start()
